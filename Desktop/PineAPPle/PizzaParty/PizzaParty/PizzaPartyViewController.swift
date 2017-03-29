10fdd04378eda4c69a0e307feee3780cb7548687
Clay Mills <AnimalHouse@Clays-MacBook-Pro.local>
2017-03-29 15:59:03 -0600

    Add pp logo

--- Desktop/PineAPPle/PizzaParty/PizzaParty/PizzaPartyViewController.swift
+++ Desktop/PineAPPle/PizzaParty/PizzaParty/PizzaPartyViewController.swift
@@ -10,17 +10,28 @@ import UIKit
 
 class PizzaPartyViewController: UIViewController {
     
-    var titlePizzaParty = UIImage()
-    var howManyAreEating = UILabel()
-    
-    var slicePerPerson = UILabel()
-    var slicePerPersonNumber
+    var pizzaPartyLogo = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor(red: 94/255, green: 178/255, blue: 242/255, alpha: 1)
-
+        
+        setupPizzaPartyLogo()
+        
+    }
+    
+    func setupPizzaPartyLogo() {
+        pizzaPartyLogo.image = #imageLiteral(resourceName: "PizzaPartyLogo")
+        self.view.addSubview(pizzaPartyLogo)
+    }
+    func setupLogoConstraints() {
+        pizzaPartyLogo.translatesAutoresizingMaskIntoConstraints = false
+        //height and width constraints
+        let logoWidth = NSLayoutConstraint(item: #imageLiteral(resourceName: "PizzaPartyLogo"), attribute: .width, relatedBy: .equal, toItem: self.view, attribute: .width, multiplier: 1, constant: 0)
+        let logoHeight = NSLayoutConstraint(item: #imageLiteral(resourceName: "PizzaPartyLogo"), attribute: .height, relatedBy: .equal, toItem: self.view, attribute: .height, multiplier: 1, constant: 0)
+    
+        self.view.addConstraints([logoWidth, logoHeight])
     }
 
 }
